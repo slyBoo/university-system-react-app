87d40be734cbcd43ebdb94f8e8cf77f6ffe280fc
Alex Petrash <alexpetrash@Olegs-MacBook-Pro.local>
2023-04-12 10:38:46 +0100

    added selectors for cohort form and student form

--- frontend/universityfrontend/src/components/cohortform.jsx
+++ frontend/universityfrontend/src/components/cohortform.jsx
@@ -1,70 +1,128 @@
 import { Button, TextField } from "@mui/material";
 import { Box } from "@mui/system";
 import Typography from "@mui/material/Typography"
+import { MenuItem, FormControl, InputLabel, Select } from "@mui/material";
+import { useState, useEffect } from "react";
 
 function CohortForm() {
+    const [degrees, setDegrees] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [selectedValues, setSelectedValues] = useState("");
+
+    const handleChange = (event) => {
+        const { value } = event.target;
+        setSelectedValues(value);
+    };
+    useEffect(() => {
+        // our code goes here
+        fetch("http://127.0.0.1:8000/api/degree/")
+            .then(response => response.json())
+            .then(data => {
+                setDegrees(data) // get the array of text out and set it as our state
+                setIsLoaded(true);
+            })
+            .catch(err => console.log(err))
+    }
+        , [])
+
+    const menuStyle = {
+        maxHeight: 200,
+        overflowY: "auto",
+        '& ul': {
+            padding: 0,
+            margin: 0
+        }
+    }
+
+    const MenuProps = {
+        MenuListProps: {
+            style: menuStyle
+        }
+    }
+    function degreeSelector() {
+        return degrees.map(degree =>
+            <MenuItem key={degree.shortcode} value={`http://127.0.0.1:8000/api/degree/${degree.shortcode}/`}>{degree.shortcode}</MenuItem>
+        )
+    }
     function handleSubmit(e) {
         // prevent the browser from reloading the page
         e.preventDefault();
         const form = e.target;
         const formData = new FormData(form);
-
+        const jsonData = {};
+        for (let [key, value] of formData.entries()) {
+            jsonData[key] = value;
+        }
+        jsonData["degree"] = selectedValues;
+        console.log(JSON.stringify(jsonData));
         // pass form data as a fetch body diretly;
-        fetch('http://127.0.0.1:8000/api/cohort/', { method: form.method, body: formData });
+        fetch('http://127.0.0.1:8000/api/cohort/', { method: form.method, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(jsonData) });
     }
-    return (
-        
-        <Box component="form" method="post" align="center" onSubmit={handleSubmit} sx={{ mt: "6rem", mx: "20em"}}>
-            <Typography variant="h2" gutterBottom sx={{
-            background: '-webkit-linear-gradient(#ee0979, #ff6a00)',
-            'WebkitBackgroundClip': 'text',
-            'WebkitTextFillColor': 'transparent',
-            backgroundClip: 'text',
-            fontWeight: 400,
-            }}>
-                Create Cohort Form
-            </Typography>
-            <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="id"
-            label="ID"
-            name="id"
-            />
-            <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="year"
-            label="Year"
-            name="year"
-            />
-            <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="degree"
-            label="Degree"
-            name="degree"
-            />
-            <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="name"
-            label="Name"
-            name="name"
-            />
+    if (isLoaded) {
+        return (
 
-            <Button
-            type="submit"
-            color="success"
-            variant="contained"
-            sx={{ display: "flex", ml: "auto", mr: 0, mt: 3, mb: 2 }}
-            >Create Cohort</Button>
-        </Box>
-    )
+            <Box component="form" method="post" align="center" onSubmit={handleSubmit} sx={{ mt: "6rem", mx: "20em" }}>
+                <Typography variant="h2" gutterBottom sx={{
+                    background: '-webkit-linear-gradient(#ee0979, #ff6a00)',
+                    'WebkitBackgroundClip': 'text',
+                    'WebkitTextFillColor': 'transparent',
+                    backgroundClip: 'text',
+                    fontWeight: 400,
+                }}>
+                    Create Cohort Form
+                </Typography>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="id"
+                    label="ID"
+                    name="id"
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="year"
+                    label="Year"
+                    name="year"
+                />
+                <FormControl fullWidth id="degree">
+                    <InputLabel id="degree">Degree</InputLabel>
+                    <Select
+                        labelId="degree"
+                        label="Degree"
+                        id="degree"
+                        required
+                        fullWidth
+                        value={selectedValues}
+                        onChange={handleChange}
+                        sx={{ mt: "1rem" }}
+
+                        MenuProps={
+                            MenuProps
+                        }
+                    >
+                        {degreeSelector()}
+                    </Select>
+                </FormControl>
+                <TextField
+                    margin="normal"
+                    fullWidth
+                    id="name"
+                    label="Name"
+                    name="name"
+                />
+
+                <Button
+                    type="submit"
+                    color="success"
+                    variant="contained"
+                    sx={{ display: "flex", ml: "auto", mr: 0, mt: 3, mb: 2 }}
+                >Create Cohort</Button>
+            </Box>
+        )
+    }
 }
 
 export default CohortForm
--- frontend/universityfrontend/src/components/studentform.jsx
+++ frontend/universityfrontend/src/components/studentform.jsx
@@ -1,68 +1,128 @@
 import { Button, TextField } from "@mui/material";
 import { Box } from "@mui/system";
 import Typography from "@mui/material/Typography"
-
+import { MenuItem, FormControl, InputLabel, Select } from "@mui/material";
+import { useState, useEffect } from "react";
 function StudentForm() {
+    const [cohorts, setCohorts] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [selectedValues, setSelectedValues] = useState("");
+
+    const handleChange = (event) => {
+        const { value } = event.target;
+        setSelectedValues(value);
+    };
+
+    const menuStyle = {
+        maxHeight: 200,
+        overflowY: "auto",
+        '& ul': {
+            padding: 0,
+            margin: 0
+        }
+    }
+
+    const MenuProps = {
+        MenuListProps: {
+            style: menuStyle
+        }
+    }
+    useEffect(() => {
+        // our code goes here
+        fetch("http://127.0.0.1:8000/api/cohort/")
+            .then(response => response.json())
+            .then(data => {
+                setCohorts(data) // get the array of text out and set it as our state
+                setIsLoaded(true);
+            })
+            .catch(err => console.log(err))
+    }
+        , [])
+
     function handleSubmit(e) {
         // prevent the browser from reloading the page
         e.preventDefault();
         const form = e.target;
         const formData = new FormData(form);
-
+        const jsonData = {};
+        for (let [key, value] of formData.entries()) {
+            jsonData[key] = value;
+        }
+        jsonData["cohort"] = selectedValues;
+        console.log(JSON.stringify(jsonData));
         // pass form data as a fetch body diretly;
-        fetch('http://127.0.0.1:8000/api/student/', { method: form.method, body: formData });
+        fetch('http://127.0.0.1:8000/api/student/', { method: form.method, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(jsonData) });
     }
-    return (
-        <Box component="form" method="post" align="center" onSubmit={handleSubmit} sx={{ mt: "6rem", mx: "20em"}}>
-            <Typography variant="h2" gutterBottom sx={{
-            background: '-webkit-linear-gradient(#ee0979, #ff6a00)',
-            'WebkitBackgroundClip': 'text',
-            'WebkitTextFillColor': 'transparent',
-            backgroundClip: 'text',
-            fontWeight: 400,
-            }}>
-                Create Student Form
-            </Typography>
-            <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="student_id"
-            label="Student ID"
-            name="student_id"
-            />
-            <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="first_name"
-            label="First name"
-            name="first_name"
-            />
-            <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="last_name"
-            label="Last name"
-            name="last_name"
-            /><TextField
-            margin="normal"
-            required
-            fullWidth
-            id="cohort"
-            label="Cohort"
-            name="cohort"
-            />
-           
-            <Button
-            type="submit"
-            color="success"
-            variant="contained"
-            sx={{ display: "flex", ml: "auto", mr: 0, mt: 3, mb: 2 }}
-            >Create Student</Button>
-        </Box>
-    )
+    function cohortSelector() {
+        return cohorts.map(cohort =>
+            <MenuItem key={cohort.id} value={`http://127.0.0.1:8000/api/cohort/${cohort.id}/`}>{cohort.name}</MenuItem>
+        )
+    }
+    if (isLoaded) {
+
+        return (
+            <Box component="form" method="post" align="center" onSubmit={handleSubmit} sx={{ mt: "6rem", mx: "20em" }}>
+                <Typography variant="h2" gutterBottom sx={{
+                    background: '-webkit-linear-gradient(#ee0979, #ff6a00)',
+                    'WebkitBackgroundClip': 'text',
+                    'WebkitTextFillColor': 'transparent',
+                    backgroundClip: 'text',
+                    fontWeight: 400,
+                }}>
+                    Create Student Form
+                </Typography>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="student_id"
+                    label="Student ID"
+                    name="student_id"
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="first_name"
+                    label="First name"
+                    name="first_name"
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="last_name"
+                    label="Last name"
+                    name="last_name"
+                />
+                <FormControl fullWidth id="cohort">
+                    <InputLabel id="cohort">Cohorts</InputLabel>
+                    <Select
+                        labelId="cohort"
+                        label="Cohort"
+                        id="cohort"
+                        required
+                        fullWidth
+                        value={selectedValues}
+                        onChange={handleChange}
+                        sx={{ mt: "1rem" }}
+
+                        MenuProps={
+                            MenuProps
+                        }
+                    >
+                        {cohortSelector()}
+                    </Select>
+                </FormControl>
+                <Button
+                    type="submit"
+                    color="success"
+                    variant="contained"
+                    sx={{ display: "flex", ml: "auto", mr: 0, mt: 3, mb: 2 }}
+                >Create Student</Button>
+            </Box>
+        )
+    }
 }
 
 export default StudentForm
